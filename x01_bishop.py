63503c4a4e72e4d591f37ffe4c6cc9c7789c3778
Valerian <124203346+Sakuretsu123@users.noreply.github.com>
2023-04-13 11:32:24 -0700

    bishop done (knight is so hard wow)

--- x01_bishop.py
+++ x01_bishop.py
@@ -9,8 +9,50 @@ def bishop(square):
   return:
   list of possible squares that the bishop can move to:
   """
-  
-  return None
+  verticalb = vertical = square[1]
+  horizontalb = horizontal = square[0]
+  myList = []
+
+  #topleft = square + 1 number - 1 letter 
+  #topright = square + 1 number + 1 letter
+  #bottomleft = square - 1 number - 1 letter 
+  #bottomright = square - 1 number + 1 letter
+
+  while vertical != 8 and horizontal != "a":
+    horizontal  = chr(ord(horizontal )-1)
+    vertical = int(vertical )+1
+    myList.append(str(horizontal)+str(vertical))
+  vertical = verticalb
+  horizontal = horizontalb
+
+  while vertical != 8 and horizontal != "h":
+    horizontal  = chr(ord(horizontal )+1)
+    vertical = int(vertical )+1
+    myList.append(str(horizontal)+str(vertical))
+  vertical = verticalb
+  horizontal = horizontalb
+
+  while vertical != 1 and horizontal != "a":
+    horizontal  = chr(ord(horizontal )-1)
+    vertical = int(vertical )-1
+    myList.append(str(horizontal)+str(vertical))
+  vertical = verticalb
+  horizontal = horizontalb
+
+  while vertical != 1 and horizontal != "h":
+    horizontal  = chr(ord(horizontal )+1)
+    vertical = int(vertical )-1
+    myList.append(str(horizontal)+str(vertical))
+  vertical = verticalb
+  horizontal = horizontalb
+
+  return myList
+
+
+
+
+
+
 
 
 def main():
@@ -19,7 +61,7 @@ def main():
   assert myList == ['a8', 'b7', 'c6', 'd1', 'd5', 'e2', 'e4', 'g2', 'g4', 'h1', 'h5']
   myList = bishop('g7')
   myList.sort()
-  assert myList = ['a1', 'b2', 'c3', 'd4', 'e5', 'f6', 'f8', 'h6', 'h8']
+  assert myList == ['a1', 'b2', 'c3', 'd4', 'e5', 'f6', 'f8', 'h6', 'h8']
 
 if __name__ == "__main__":
   main()
